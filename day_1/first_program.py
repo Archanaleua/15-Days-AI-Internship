c68babc82dc4e8bb0fc4d0a6b12052904792723a
ARCHANA <archanaleua09@gmail.com>
2026-05-11 16:26:28 +0530

    Day 1 completed

---
+++ day_1/first_program.py
@@ -0,0 +1,22 @@
+print("Hello Archana!")
+print("Welcome to Data Science Internship")
+
+#first python program 
+#SIMPLE PASSWORD CHECKER
+
+correct_password = "python123"
+print("WELCOME TO LOGIN SYSTEM")
+password = input("Enter your password: ")
+
+if password == correct_password:
+    
+    print("Login Successful!")
+    print("Welcome User")
+
+    print("Loading...")
+    print("System Ready")
+
+else:
+
+    print("Wrong Password!")
+    print("Access Denied")
