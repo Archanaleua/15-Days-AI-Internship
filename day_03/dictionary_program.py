60febb70ef5893b24485d05768a41df01b5dc6eb
Archana <archanaleua09@gmail.com>
2026-05-13 23:37:15 +0530

    Day 3 data structures completed

---
+++ day_03/dictionary_program.py
@@ -0,0 +1,15 @@
+print("===== DICTIONARY PROGRAM =====")
+
+student = {
+    "name": "Archana",
+    "age": 20,
+    "course": "AI"
+}
+
+print(student)
+
+print(student["name"])
+
+student["age"] = 21
+
+print(student)
---
+++ day_03/tuple_program.py
@@ -0,0 +1,9 @@
+print("===== TUPLE PROGRAM =====")
+
+colors = ("Red", "Blue", "Green")
+
+print(colors)
+
+print(colors[1])
+
+print(len(colors))
